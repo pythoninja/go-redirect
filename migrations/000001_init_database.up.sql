3fb64eab6ce36c81dd7aba6e03f8b4c9e49c6d71
pythoninja <pythoninja@users.noreply.github.com>
2024-05-29 16:38:00 +0300

    fix(migration): add unique constraint on alias field

--- migrations/000001_init_database.up.sql
+++ migrations/000001_init_database.up.sql
@@ -4,7 +4,7 @@ create table if not exists links
 (
   id         bigserial primary key,
   created_at timestamp with time zone not null default now(),
-  alias      text                     not null,
+  alias      text                     not null unique,
   long_url   text                     not null,
   clicks     int                      not null default 0
 );
