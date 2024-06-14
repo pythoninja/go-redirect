2c30ed0cd552865d0f2564cbe321fc7300a6684b
pythoninja <pythoninja@users.noreply.github.com>
2024-06-14 09:49:32 +0300

    refactor(router): replace `chi.UrlParam` with `PathValue`

--- internal/api/helpers.go
+++ internal/api/helpers.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"errors"
-	"github.com/go-chi/chi/v5"
 	"net/http"
 	"strconv"
 	"strings"
@@ -12,7 +11,7 @@ import (
 // It returns the parsed "id" as int64 if it is a positive integer, otherwise
 // an error with message "invalid id parameter".
 func readIDParam(r *http.Request) (int64, error) {
-	id, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 64)
+	id, err := strconv.ParseInt(r.PathValue("id"), 10, 64)
 	if err != nil || id < 1 {
 		return 0, errors.New("invalid id parameter")
 	}
@@ -22,7 +21,7 @@ func readIDParam(r *http.Request) (int64, error) {
 
 // readAliasParam extracts and returns the value of the "alias" parameter from the request URL.
 func readAliasParam(r *http.Request) string {
-	return chi.URLParam(r, "alias")
+	return r.PathValue("alias")
 }
 
 func normalizeAlias(alias string) string {
